eb1549bd1705bddf20c7564a20ba852cbad96c4b
CelestialCrafter <celestialcraftermc@gmail.com>
2024-04-06 00:08:32 -0500

    bluetooth

--- configuration.nix
+++ configuration.nix
@@ -134,6 +134,17 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+  services.pipewire.wireplumber.configPackages = [
+    (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+      bluez_monitor.properties = {
+        ["bluez5.enable-sbc-xq"] = true,
+        ["bluez5.enable-msbc"] = true,
+        ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+      }
+    '')
+  ];
+
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
