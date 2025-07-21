33ecc64bd0b6efcc3de0fc3404bf643a7c2d5ac6
CelestialCrafter <celestialcraftermc@gmail.com>
2025-07-21 05:03:31 -0500

    (helix) svelte support

--- modules/desktop/home/helix.nix
+++ modules/desktop/home/helix.nix
@@ -8,14 +8,15 @@
 
     extraPackages = with pkgs; [
       ruff # python
-      lua-language-server # lua
+      stylua lua-language-server # lua
       rust-analyzer rustfmt rustc cargo # rust
       gopls golangci-lint golangci-lint-langserver go # go
       nil nixfmt-rfc-style # nix
       typescript-language-server # javascript
-      nodePackages.prettier # javascript, html, css, scss, json
+      nodePackages.prettier # javascript, html, css, scss, json, svelte
       marksman mdformat # marksman
       clang clang-tools # c
+      svelte-language-server # svelte
     ];
   };
 }
