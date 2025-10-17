7335e085286653fbdaab8e547e2c6baa6b8db3b1
CelestialCrafter <celestialcraftermc@gmail.com>
2025-10-17 13:00:49 -0500

    disable auto gc

--- modules/nix-settings.nix
+++ modules/nix-settings.nix
@@ -1,21 +1,11 @@
-{ pkgs, ... }:
-
 {
   nixpkgs.config.allowUnfree = true;
 
-  nix = {
-    settings = {
-      auto-optimise-store = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-    };
-
-    optimise.automatic = true;
-    gc = {
-      automatic = true;
-      options = "--delete-older-than 7d";
-    };
+  nix.settings = {
+    auto-optimise-store = true;
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
   };
 }
